3fc0f647ee74a99926a363cb67bb54ba8d46be3d
Andres Suarez <andres27059934@gmail.com>
2019-05-26 16:56:29 -0300

    Added link function

--- src/getVideo.js
+++ src/getVideo.js
@@ -3,7 +3,8 @@ var url="https://www.youtube.com/watch?v=CfKVU0i7w_w&list=PLU8oAlHdN5BmpobVmj1Il
 var i;
 var titlesArray = [];
 var thumbnailsArray = [];
-var tempToTest = 5;
+var linksArray = [];
+var j;
 
 
 function titleArrayParse(text, initialFlag, finalFlag){
@@ -15,6 +16,19 @@ function titleArrayParse(text, initialFlag, finalFlag){
 function thumbnailArrayParse(text, initialFlag, finalFlag,offset){
 
 	return text[i].substring(text[i].indexOf(initialFlag,offset)+6, text[i].indexOf(finalFlag,offset));
+
+}
+
+function videoLinksArrayParse(text){
+	var temp1= thumbnailArrayParse(text, '"url":',',"width":',266+titlesArray[i].length);
+	temp1.replace('"','') //remove quotes from link
+	
+	temp1 = temp1.substring(24,temp1.indexOf('/hqdefault'));
+	
+	var temp2 = 'https://www.youtube.com/watch?v='+temp1;
+	//that's the link without the playlist, for now that's the only way
+	return temp2
+
 
 }
 
@@ -33,11 +47,9 @@ function analyze(response){
 	}
 
 	for(i=0;i<sourceArray.length;i++){
-		//alert(thumbnailsArray[i]);
+		linksArray[i] = videoLinksArrayParse(sourceArray)
 	}
 
-	
-//	alert(titlesArray[0]);
 }
 
 
@@ -45,7 +57,6 @@ function check(){
 
 		$.get(url, function(responseText) {
     		analyze(responseText);
-			alert("Check "+titlesArray[0]);
 		});
 }
 	
