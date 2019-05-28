76f684f3beec1a7e09153446ca61f3ae4e06ce84
andylarquy <andres27059934@gmail.com>
2019-05-27 22:08:56 -0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -5,14 +5,7 @@
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-        /*Con estas condiciones me aseguro que solo se pueda activar el popup
-          Si la pagina es youtube y tiene ese codigo CSS especifico de las listas de reporudccion.
-        */
-
-        /*Whit these conditions I make me sure that the popup can only  be activated
-          If the page is youtube and it has that CSS code that's specific of the 'reporduction lists' ESL.
-        */
-
+          
         css:["ytd-playlist-panel-video-renderer"]
         
         })
